43e7c06ecdb0f36996f54facebba4878aa5a5155
lian <lianxiaoyang.happy@163.com>
2019-01-26 21:57:07 +0800

    first commit

---
+++ t1_two_sum/t1_two_sum.go
@@ -0,0 +1,13 @@
+package t1_two_sum
+
+func twoSum(nums []int, target int) []int {
+	for idx, v := range nums {
+		for i := idx+1; i < len(nums); i++ {
+			if v + nums[i] == target {
+				return []int{idx, i}
+			}
+		}
+	}
+	return []int{}
+}
+
---
+++ t2_add_two_numbers/t2_add_two_numbers.go
@@ -0,0 +1,58 @@
+package t2_add_two_numbers
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	if nil == l1 {
+		return l2
+	}
+	if nil == l2 {
+		return l1
+	}
+	h := &ListNode{}
+	p, p1, p2 := h, l1, l2
+	j := 0
+	for nil != p1 && nil != p2 {
+		s := p1.Val + p2.Val + j
+		p.Val = s%10
+		j = s/10
+
+		p1, p2 = p1.Next, p2.Next
+		if nil != p1 || nil != p2 {
+			p.Next = &ListNode{}
+			p = p.Next
+		}
+	}
+
+	if p2 != nil { p1 = p2}
+
+	for nil != p1 {
+		s := p1.Val + j
+		p.Val = s%10
+		j = s/10
+
+		p1 = p1.Next
+		if nil != p1 {
+			p.Next = &ListNode{}
+			p = p.Next
+		}
+	}
+
+	if j > 0 {
+		p.Next = &ListNode{}
+		p = p.Next
+		p.Val = j
+	}
+
+	return h
+}
